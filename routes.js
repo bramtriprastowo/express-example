2950a1598b391692715cbbdee6f1b60ffc359e96
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2022-09-29 17:14:37 +0700

    change routes and add html file

--- routes.js
+++ routes.js
@@ -5,39 +5,28 @@ const fs = require("fs");
 const path = require("path");
 
 router.get("/", (req, res) => {
-  const { page, total } = req.query;
-  res.send({
+  res.sendFile("views/home.html", { root: __dirname });
+});
+
+router.get("/json", (req, res) => {
+  res.json({
     status: "Success",
     message: "Welcome to Express JS",
+  });
+});
+
+router.get("/query", (req, res) => {
+  const { page } = req.query;
+  res.send({
     page,
-    total,
   });
 });
 
 router.get("/product/:id", (req, res) => {
   res.send({
     id: req.params.id,
+    produk: `Produk ${req.params.id}`,
   });
 });
-
-router.post("/product", upload.single("image"), (req, res) => {
-  const { name, price } = req.body;
-  const image = req.file;
-  if (image) {
-    const target = path.join(__dirname, "uploads", image.originalname);
-    fs.renameSync(image.path, target);
-    // res.json({
-    //   name,
-    //   price,
-    //   image,
-    // });
-    res.sendFile(target);
-  }
-});
-
-// router.get("/:category/:tag", (req, res) => {
-//   const { category, tag } = req.params;
-//   res.json({ category, tag });
-// });
 
 module.exports = router;
